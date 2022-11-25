b3e50f35097ae09f6112140a4d00411bbca80400
Sujo028 <sujokoshyjohn2040@gmail.com>
2022-11-25 12:40:48 +0530

    adding feature to enter the size of the array

--- gitVCS/file1.java
+++ gitVCS/file1.java
@@ -1,6 +1,10 @@
+java.util.*;
 public class file1{
     public static void main(String args[]){
         // System.out.println("First file to do hands-on on Git(Version Control System).");
         System.out.println("Program to find a sum n number of an array");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Size of the array:");
+        int n=sc.nextInt();
     }
 }
