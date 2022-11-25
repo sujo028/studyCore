e33432a51c18ed917e4d5d77a40b713aed03588d
Sujo028 <sujokoshyjohn2040@gmail.com>
2022-11-25 12:29:23 +0530

    defined the printing statement of the purpose of program

--- gitVCS/file1.java
+++ gitVCS/file1.java
@@ -1,5 +1,6 @@
 public class file1{
     public static void main(String args[]){
-        System.out.println("First file to do hands-on on Git(Version Control System).");
+        // System.out.println("First file to do hands-on on Git(Version Control System).");
+        System.out.println("Program to find a sum n number of an array");
     }
 }
