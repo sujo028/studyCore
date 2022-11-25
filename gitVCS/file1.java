a69d07250c029868a0587d3cd4ffbac7844b2eff
Sujo028 <sujokoshyjohn2040@gmail.com>
2022-11-25 14:36:28 +0530

    recieving array data from user

--- gitVCS/file1.java
+++ gitVCS/file1.java
@@ -6,5 +6,9 @@ public class file1{
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the Size of the array:");
         int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
     }
 }
